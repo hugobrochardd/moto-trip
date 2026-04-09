3ea03135ca801a4282538e8158772956d897d61d
hugobrochardd <hugo.brochard3@icloud.com>
2026-04-09 13:41:21 +0200

    first push

---
+++ src/main/java/com/example/mototripeval/MotoTripEvalApplication.java
@@ -0,0 +1,13 @@
+package com.example.mototripeval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoTripEvalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotoTripEvalApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/mototripeval/User.java
@@ -0,0 +1,24 @@
+@Entity
+public class User {
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+    private boolean premium;
+    private int points;
+
+    public User() {}
+
+    public User(String name, boolean premium) {
+        this.name = name;
+        this.premium = premium;
+        this.points = 0;
+    }
+
+    public void addPoints(int pts) {
+        this.points += pts;
+    }
+
+    public boolean canJoinPremium() {
+        return premium;
+    }
+}
